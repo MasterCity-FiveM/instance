290ec94e992d0c0e6a0886854fafd9e16119482c
MasterkinG32 <lichwow_masterking@yahoo.com>
2021-02-23 21:08:35 -0800

    Translate to Persian, @AmirhN.

--- locales/en.lua
+++ locales/en.lua
@@ -1,8 +1,8 @@
 Locales['en'] = {
-  ['left_instance'] = 'you have left the instance',
-  ['invite_expired'] = 'invite expired',
-  ['press_to_enter'] = 'press ~INPUT_CONTEXT~ to enter the instance',
-  ['entered_instance'] = 'you entered the instance',
-  ['entered_into'] = '%s entered the instance',
-  ['left_out'] = '%s left the instance',
+  ['left_instance'] = 'شما از خانه خارج شدید.',
+  ['invite_expired'] = 'دعوت منقضی شد.',
+  ['press_to_enter'] = 'دکمه [E] را جهت ورود به خانه فشار دهید.',
+  ['entered_instance'] = 'شما وارد خانه شدید.',
+  ['entered_into'] = '%s وارد خانه شد.',
+  ['left_out'] = '%s از خانه خارج شد.',
 }
